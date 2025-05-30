31f548f3752688fb19ebce6bfea4f2984135a37d
Askar Kasimov <kasimovaskar123@gmail.com>
2025-05-30 18:28:40 +0300

    redux removed

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.scss';
 import App from './App.tsx';
-import { Provider } from 'react-redux';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { store } from './store';
 import { ToastContainer } from 'react-toastify';
 import Home from './pages/Home';
 import Additional from './pages/Additional';
@@ -28,7 +26,6 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider store={store}>
       <RouterProvider router={router} />
       <ToastContainer
         position="bottom-right"
@@ -40,6 +37,5 @@ createRoot(document.getElementById('root')!).render(
         pauseOnHover
         theme="light"
       />
-    </Provider>
   </StrictMode>
 );
